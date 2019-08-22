46798f7d798f41bad9f498f485939a83f537e08d
Chris Ellis <ellis.cs@gmail.com>
2019-08-22 06:28:14 -0400

    Signup form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,35 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     High
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="bottyMcBotface"
+    >
+      <p className="hidden">
+        <label htmlFor="bottyMcBotface">
+          Dear Human, please don't fill this out:{' '}
+          <input name="bottyMcBotface" />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="email">
+          <input type="email" name="email" placeholder="Your email address" />
+        </label>
+      </p>
+
+      <p>
+        <button type="submit">Subscribe</button>
+      </p>
+    </form>
   </Layout>
 )
 
