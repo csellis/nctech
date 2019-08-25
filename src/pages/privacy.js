b31d640617220c3540f228fe16d32a10152db391
Chris Ellis <ellis.cs@gmail.com>
2019-08-25 07:33:05 -0400

    Start privacy page

---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/pageLayout'
+
+const Privacy = () => (
+  <Layout>
+    <div className="flex flex-row">
+      <h1>Privacy</h1>
+    </div>
+  </Layout>
+)
+
+export default Privacy
