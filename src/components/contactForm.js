c32cb049e0e5997ea95c309f771b7f023b9f3510
Chris Ellis <ellis.cs@gmail.com>
2019-08-25 07:33:38 -0400

    Make contact form controlled, controlled post, email validation

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,55 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const contactForm = () => (
-  <div className="flex-1 text-center p-6 font-sans self-center">
-    <h1 className="font-bold inline-block px-8 bg-orange-300 text-4xl">
-      NC Tech Scene
-    </h1>
-    <h2 className="text-xl mt-12">
-      A weekly email newsletter about tech in North Carolina.
-    </h2>
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bottyMcBotface"
-      className="mt-8"
-    >
-      <p className="hidden">
-        <label htmlFor="bottyMcBotface">
-          Dear Human, please don't fill this out:{' '}
-          <input type="hidden" name="bottyMcBotface" />
-          <input type="hidden" name="form-name" value="contact" />
-        </label>
+const encode = data =>
+  Object.keys(data)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+    .join('&')
+
+const contactForm = () => {
+  const [form, setForm] = useState({
+    email: '',
+    emailValid: false,
+  })
+
+  const validateEmail = () => {
+    const reg = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    if (reg.test(form.email)) {
+      return true
+    }
+    return false
+  }
+
+  const updateField = e => {
+    const { name, value } = e.target
+    const emailValid = validateEmail()
+    setForm({
+      ...form,
+      [name]: value,
+      emailValid,
+    })
+  }
+
+  const handleSubmit = e => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', form }),
+    })
+      .then(() => alert('Success!'))
+      .catch(error => alert(error))
+
+    e.preventDefault()
+  }
+
+  return (
+    <div className="flex-1 text-center p-6 font-sans self-center">
+      <h1 className="font-bold inline-block px-8 bg-orange-300 text-4xl">
+        NC Tech Scene
+      </h1>
+      <h2 className="text-xl mt-12">
+        A weekly email newsletter about tech in North Carolina.
+      </h2>
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bottyMcBotface"
+        className="mt-8"
+        onSubmit={handleSubmit}
+      >
+        <p className="hidden">
+          <label htmlFor="bottyMcBotface">
+            Dear Human, please don't fill this out:{' '}
+            <input type="hidden" name="bottyMcBotface" />
+            <input type="hidden" name="form-name" value="contact" />
+          </label>
+        </p>
+        <p>
+          <label
+            htmlFor="email"
+            className="flex content-center items-center flex-col"
+          >
+            <input
+              type="email"
+              name="email"
+              placeholder="Email address"
+              value={form.email}
+              onChange={updateField}
+              className="p-4 border block border-gray-600 flex-initial w-1/2 placeholder-gray-600::placeholder font-sans text-lg"
+            />
+          </label>
+        </p>
+
+        <p>
+          <button
+            className={`bg-orange-300 hover:bg-orange-200 font-sans text-lg font-bold py-2 px-4 rounded m-4 ${
+              form.emailValid ? '' : 'cursor-not-allowed opacity-50'
+            }`}
+            type="submit"
+          >
+            Subscribe
+          </button>
+        </p>
+      </form>
+
+      <p className="text-lg">
+        Join other North Carolinians interested in Tech.
       </p>
-      <p>
-        <label
-          htmlFor="email"
-          className="flex content-center items-center flex-col"
-        >
-          <input
-            type="email"
-            name="email"
-            placeholder="Your email address"
-            className="p-4 border block border-gray-600 flex-initial w-1/2 placeholder-gray-600::placeholder font-sans text-lg"
-          />
-        </label>
-      </p>
 
-      <p>
-        <button
-          className="bg-orange-300 hover:bg-orange-200 font-sans text-lg font-bold py-2 px-4 rounded m-4"
-          type="submit"
-        >
-          Subscribe
-        </button>
-      </p>
-    </form>
-
-    <p className="text-lg">Join other North Carolinians interested in Tech.</p>
-
-    <p className="mt-8">Published by Simple Creative Webdevelopment</p>
-  </div>
-)
+      <p className="mt-8">Published by Simple Creative Webdevelopment</p>
+    </div>
+  )
+}
 
 export default contactForm
