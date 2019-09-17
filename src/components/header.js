045137114cbd64a5f124277be6fd09bcef0f8b38
Chris Ellis <ellis.cs@gmail.com>
2019-09-17 06:52:19 -0400

    Page layout css

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Header = ({ siteTitle }) => (
-  <header className="mb-4 pt-4 pb-4 bg-orange-300 font-sans">
-    <div className="container mx-auto">
-      <h1 className="font-bold py-4">
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-    </div>
+  <header className="header">
+    <nav>
+      <div className="logo">
+        <h1>
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+      </div>
+      <div className="navigation"></div>
+    </nav>
   </header>
 )
 
--- src/components/pageLayout.js
+++ src/components/pageLayout.js
@@ -1,46 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql, Link as RouterLink } from 'gatsby'
-import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Toolbar from '@material-ui/core/Toolbar'
-import IconButton from '@material-ui/core/IconButton'
-import SearchIcon from '@material-ui/icons/Search'
-import Paper from '@material-ui/core/Paper'
-import Typography from '@material-ui/core/Typography'
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Hidden from '@material-ui/core/Hidden'
-import Link from '@material-ui/core/Link'
-import Button from '@material-ui/core/Button'
-import Divider from '@material-ui/core/Divider'
-import AppBar from '@material-ui/core/AppBar'
-
-import { ThemeProvider } from '@material-ui/styles'
-import theme from '../theme'
 
 import SEO from './seo'
 import Header from './header'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-}))
+import '../app.scss'
 
 const PageLayout = ({ children }) => {
-  const classes = useStyles()
-
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -52,27 +19,11 @@ const PageLayout = ({ children }) => {
   `)
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="absolute">
-          <Toolbar>
-            <Typography component="h1" variant="h6">
-              <Link component={RouterLink} to="/" color="inherit">
-                {data.site.siteMetadata.title}
-              </Link>
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <SEO />
-        <Container maxWidth="lg">
-          <main className={classes.content}>
-            <div className={classes.appBarSpacer}></div>
-            {children}
-          </main>
-        </Container>
-      </div>
-    </ThemeProvider>
+    <div className="">
+      <SEO />
+      <Header siteTitle={data.site.siteMetadata.title} />
+      <main className="content">{children}</main>
+    </div>
   )
 }
 
