dd673682438fcb1a2c63d98e2be812656b5cbf08
Chris Ellis <ellis.cs@gmail.com>
2019-09-30 03:10:47 -0400

    Update success message

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,7 +6,12 @@ import Layout from '../components/pageLayout'
 const Success = () => (
   <Layout>
     <div className="flex flex-row">
-      <h1>Success!</h1>
+      <h1>Great, one last thing...</h1>
+
+      <h2>We just sent you an email with a confirmation link.</h2>
+      <h2>
+        <strong>Please check your inbox!</strong>
+      </h2>
     </div>
   </Layout>
 )
