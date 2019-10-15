dda0b1687e1abc4c34383ed62894c96912fcd112
Chris Ellis <ellis.cs@gmail.com>
2019-10-15 06:03:57 -0400

    Add title into Issue page

--- src/templates/issues.js
+++ src/templates/issues.js
@@ -10,17 +10,28 @@ export default ({ data }) => {
 
   console.log(topIssueNumber)
 
-  const prevNumber = post.issue === 1 ?  <div></div> : <Link to={`/issue/${post.issue -1}`}>« Prev</Link>;
-  const nextNumber = post.issue === topIssueNumber ? <div></div> : <Link to={`/issue/${post.issue + 1}`}>Next »</Link>
+  const prevNumber = post.issue === 1 ?  <></> : <Link to={`/issue/${post.issue -1}`}>« Prev</Link>;
+  const nextNumber = post.issue === topIssueNumber ? <></> : <Link to={`/issue/${post.issue + 1}`}>Next »</Link>
 
   return (
-      <div style={{ display: 'flex', flexDirection: 'column'}}>
-        <div style={{ height: '40px'}}>
-          {prevNumber}
-          {nextNumber}
+      <div style={{ display: 'flex', flexDirection: 'column', borderTop:'8px solid #fbd38d'}}>
+        <nav style={{ width: '600px', margin: 'auto'}}>
+          <div style={{ display: 'inline-block', background: '#fbd38d', textDecoration: 'none', padding: '0.75rem 0 0.75rem 0'}}>
+              <Link to="/" style={{ textDecoration: 'none', color: '#333', fontSize: '2rem', fontWeight: '700'}}>
+                NC Tech Scene
+              </Link>
+          </div>
+        </nav>
+        <div style={{ height: '40px', display: 'flex', justifyContent: 'space-between', width: '600px', margin: 'auto', alignItems: 'center'}}>
+          <div>
+            {prevNumber}
+          </div>
+          <div>
+            {nextNumber}
+          </div>
         </div>
         <iframe
-            style={{ flex: 1, minHeight: 'calc(100vh - 40px)', border: 'none' }}
+            style={{ flex: 1, minHeight: 'calc(100vh - 40px - 8px)', border: 'none' }}
             title="Iphone Preview"
             src={post.html}
           ></iframe>
